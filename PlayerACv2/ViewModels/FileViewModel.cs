3e0438bcca8b619f5862f1a7c4165e2ecef55d79
chaupinan <antoine.chaupin-ext@vinci-autoroutes.com>
2018-06-20 16:10:35 +0200

    Mise en place des bases du projet

---
+++ PlayerACv2/ViewModels/FileViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MixerACv2.ViewModels
+{
+    public class FileViewModel : BaseViewModel
+    {
+        private string _folderPath;
+        public string FolderPath
+        {
+            get { return _folderPath; }
+            set
+            {
+                _folderPath = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _oldName;
+        public string OldName
+        {
+            get { return _oldName; }
+            set
+            {
+                _oldName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _newName;
+        public string NewName
+        {
+            get { return _newName; }
+            set
+            {
+                _newName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public FileViewModel(string filePath)
+        {
+            FolderPath = Path.GetDirectoryName(filePath);
+            OldName = Path.GetFileName(filePath);
+            NewName = Path.GetFileName(filePath);
+        }
+    }
+}
---
+++ PlayerACv2/ViewModels/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace PlayerACv2.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        #region Properties
+
+
+        #endregion
+
+        public MainViewModel()
+        {
+        }
+
+        #region Commands
+
+
+        #endregion region
+
+        #region Private methods
+
+
+        #endregion region
+    }
+}
